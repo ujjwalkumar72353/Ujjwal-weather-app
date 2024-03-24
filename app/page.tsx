6381e012176ea65fc8da9a9191f6c922f825d85d
ujjwalkumar7498 <ujjwalkumar4876@gmail.com>
2024-03-24 20:37:38 +0530

    sunset done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+
 import AirPollution from "./Components/AirPollution/AirPollution";
 import Navbar from "./Components/Navbar";
+import Sunset from "./Components/Sunset/Sunset";
 import Tempearture from "./Components/Temperature/Tempearture";
 
 
@@ -14,8 +16,9 @@ export default function Home() {
 
              <div className="flex flex-col w-full">
               <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4"> 
+              
                  <AirPollution/>
-                
+                 <Sunset/>
               </div>
              </div>
           </div>
--- app/utilis/misc.tsx
+++ app/utilis/misc.tsx
@@ -1,6 +1,16 @@
+import moment from "moment";
+
 export const kelvinToCelsius = (kelvin: number) => {
     return Math.round(kelvin - 273.15);
   };
+
+  export const unixToTime = (unix: number, timezone: number) => {
+    return moment
+      .unix(unix)
+      .utcOffset(timezone / 60)
+      .format("HH:mm");
+  };
+  
   
 
   export const airQualityIndexText = [
